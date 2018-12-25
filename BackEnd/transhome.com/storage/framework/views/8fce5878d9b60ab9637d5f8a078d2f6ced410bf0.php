3305759ce6f9165862a5513543933a044adc90be
Bui Ngoc Minh <minhnora98@gmail.com>
2018-12-25 20:41:43 +0700

    .gitignore is now working

--- BackEnd/transhome.com/storage/framework/views/8fce5878d9b60ab9637d5f8a078d2f6ced410bf0.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('title','Dashbroad'); ?>
-<?php $__env->startSection('css'); ?>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css"/>
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-<?php $__env->stopSection(); ?>
-<?php $__env->startSection('content'); ?>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('admin.layouts.app', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
